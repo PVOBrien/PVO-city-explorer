b02826a7c29fb8566af3fe68454660c3607f355c
Paul OBrien <pvovideo@outlook.com>
2020-07-13 16:18:00 -0700

    add GET request for location: working locally

--- server.js
+++ server.js
@@ -13,3 +13,27 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`listening to ${PORT}`);
 });
+
+app.get('/location', (request, response) => {
+  try{
+    console.log(request.query.city);
+    let city = request.query.city;
+    let geoData = require('./data/location.json');
+
+    const obj = new Location(city, geoData);
+
+    response.send(obj);
+
+  } catch(error){
+    console.log('Error', error);
+    response.status(500).send('we made a boo-boo! sori!');
+  }
+
+  function Location(location,geoData){
+    this.search_query = location;
+    this.formatted_query = geoData[0].display_name;
+    this.latitude = geoData[0].lat;
+    this.longitude = geoData[0].lon;
+  }
+
+});
